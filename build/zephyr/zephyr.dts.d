104577a9b1d2024aae5fa64118458e72e59509df
yanntalhouarne <talhouarne.yann@gmail.com>
2023-07-14 16:44:24 -0500

    First commit

---
+++ build/zephyr/zephyr.dts.d
@@ -0,0 +1,21 @@
+empty_file.o: /home/yann/zephyrproject/zephyr/misc/empty_file.c \
+ /home/yann/zephyrproject/zephyr/boards/arm/nrf52840dk_nrf52840/nrf52840dk_nrf52840.dts \
+ /home/yann/zephyrproject/zephyr/dts/arm/nordic/nrf52840_qiaa.dtsi \
+ /home/yann/zephyrproject/zephyr/dts/common/mem.h \
+ /home/yann/zephyrproject/zephyr/dts/arm/nordic/nrf52840.dtsi \
+ /home/yann/zephyrproject/zephyr/dts/arm/armv7-m.dtsi \
+ /home/yann/zephyrproject/zephyr/dts/common/skeleton.dtsi \
+ /home/yann/zephyrproject/zephyr/dts/arm/nordic/nrf_common.dtsi \
+ /home/yann/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/adc.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/dt-bindings/adc/nrf-adc.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/dt-bindings/dt-util.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/sys/util_macro.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/sys/util_internal.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/sys/util_loops.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/dt-bindings/i2c/i2c.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/dt-bindings/pinctrl/nrf-pinctrl.h \
+ /home/yann/zephyrproject/zephyr/include/zephyr/dt-bindings/pwm/pwm.h \
+ /home/yann/zephyrproject/zephyr/dts/common/freq.h \
+ /home/yann/zephyrproject/zephyr/dts/arm/nordic/override.dtsi \
+ /home/yann/zephyrproject/zephyr/boards/arm/nrf52840dk_nrf52840/nrf52840dk_nrf52840-pinctrl.dtsi
